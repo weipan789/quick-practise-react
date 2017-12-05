6ee988eaac3836b57cb80ca7fa8725e6fbd74e24
wp-mac <weipan789@gmail.com>
2017-12-05 21:46:01 +0800

    basic rendering elements

--- src/index.js
+++ src/index.js
@@ -6,3 +6,21 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
+
+
+
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('rendering-elements')
+  );
+}
+
+setInterval(tick, 1000);
